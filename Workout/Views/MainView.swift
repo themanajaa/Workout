233e9a6d65fe6628a665a7cfbea6e8286a6e55b1
Sami <themanajaa@gmail.com>
2022-03-03 09:55:10 +0100

    a

--- Workout/Views/MainView.swift
+++ Workout/Views/MainView.swift
@@ -10,7 +10,20 @@ import SwiftUI
 struct MainView: View {
     var body: some View {
         TabView{
+            TimerView()
+                .tabItem{
+                    Label("Timer", systemImage: "timer")
+                }
             
+            WorkoutView()
+                .tabItem{
+                    Label("Workout", systemImage: "list.dash")
+                }
+        
+            ProfileView()
+                .tabItem{
+                    Label("Profile", systemImage: "person.fill")
+                }
         }
     }
 }
