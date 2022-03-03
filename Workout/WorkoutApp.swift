d31c5db24cd06abd7820d04f8a4c5930700722c2
Sami <themanajaa@gmail.com>
2022-03-03 09:22:46 +0100

    Initial Commit

---
+++ Workout/WorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutApp.swift
+//  Workout
+//
+//  Created by abdelmlek sami on 03/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
