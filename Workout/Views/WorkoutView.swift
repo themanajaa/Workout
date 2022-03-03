6d2dda4adf802383292bf76c0214d62b0ec2fd6a
Bonlver <wankertyboniver@gmail.com>
2022-03-03 11:50:32 +0100

    Map Workout

--- Workout/Views/WorkoutView.swift
+++ Workout/Views/WorkoutView.swift
@@ -6,12 +6,35 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct WorkoutView: View {
+    
+    @State private var location = MKCoordinateRegion(center: .init(latitude: 35.677735, longitude: 139.764740), latitudinalMeters: 500, longitudinalMeters: 500) //Affichage autour des coordonnées à afficher, réglage de la plage et définition des coordonnées
+
     var body: some View {
-        Text("WorkoutView")
+        VStack{
+            
+            HStack{
+                Text("🚲 3 mars 2022")
+                    .frame( alignment: .topLeading)
+                
+            }
+            
+            Map(coordinateRegion: self.$location)
+                    .frame(width: 400, height: 500)
+            
+            
+            
+            Spacer()
+            
+            
+        }
     }
 }
+                    
+                   
+
 
 struct WorkoutView_Previews: PreviewProvider {
     static var previews: some View {
