43ae332a8db67f37e272b324df5e89634c1c2f34
Sami <themanajaa@gmail.com>
2022-03-03 09:43:01 +0100

    a

---
+++ Workout/Views/TimerView.swift
@@ -0,0 +1,20 @@
+//
+//  TimerView.swift
+//  Workout
+//
+//  Created by abdelmlek sami on 03/03/2022.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TimerView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimerView()
+    }
+}
---
+++ Workout/Views/WorkoutView.swift
@@ -0,0 +1,20 @@
+//
+//  WorkoutView.swift
+//  Workout
+//
+//  Created by abdelmlek sami on 03/03/2022.
+//
+
+import SwiftUI
+
+struct WorkoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WorkoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        WorkoutView()
+    }
+}
