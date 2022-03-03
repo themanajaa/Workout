850e05638413492abe0126de588222f80b1b1239
Bonlver <wankertyboniver@gmail.com>
2022-03-03 10:12:18 +0100

    profileView update

--- Workout/Views/ProfileView.swift
+++ Workout/Views/ProfileView.swift
@@ -2,14 +2,47 @@
 //  ProfileView.swift
 //  Workout
 //
-//  Created by abdelmlek sami on 03/03/2022.
+//  Created by bernhard william on 03/03/2022.
 //
 
 import SwiftUI
 
 struct ProfileView: View {
+    
+    @State var firstname: String = ""
+    @State var lastname: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack{
+            Text("Profile")
+            TextField("First Name", text: $firstname)
+                .padding(.horizontal)
+                .frame(height: 55)
+                .background(Color(.systemGray4))
+                .cornerRadius(10)
+            TextField("Last Name", text: $lastname)
+                .padding(.horizontal)
+                .frame(height: 55)
+                .background(Color(.systemGray4))
+                .cornerRadius(10)
+            
+            Button{
+                
+            } label:{
+                Text("Save")
+                    .foregroundColor(.white)
+                    .font(.headline)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.accentColor)
+                    .cornerRadius(10)
+            }
+            
+            Spacer()
+        }
+        .padding(14)
+        .navigationTitle("🤯 Add an Expense")
     }
 }
 
@@ -18,3 +51,4 @@ struct ProfileView_Previews: PreviewProvider {
         ProfileView()
     }
 }
+
